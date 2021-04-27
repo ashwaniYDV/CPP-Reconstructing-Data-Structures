7a9731e2a436f9cddcc2ac8f9e4ce8e9914811da
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-27 21:16:46 +0530

    Create Vector.cpp

---
+++ Vector/Vector.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include "Vector.h"
+
+template<typename T>
+void PrintVector(const Vector<T>& vector) {
+    for (size_t i = 0; i < vector.Size(); i++) {
+        std::cout << vector[i] << std::endl;
+    }
+    std::cout << "------------------" << std::endl;
+}
+
+int main() {
+    Vector<std::string> data1;
+    data1.PushBack("ashwani");
+    data1.PushBack("yadav");
+    data1.EmplaceBack("C++");
+    std::cout << data1.Size() << std::endl;
+    PrintVector(data1);
+    data1.PopBack();
+    std::cout << data1.Size() << std::endl;
+    PrintVector(data1);
+    data1.Clear();
+    std::cout << data1.Size() << std::endl;
+    PrintVector(data1);
+
+    std::cout << std::endl << std::endl;
+
+    Vector<std::string> data2(3);
+    std::cout << data2.Size() << std::endl;
+    PrintVector(data2);
+    data2[0] = "ashwani";
+    data2[1] = "yadav";
+    data2[2] = "C++";
+    std::cout << data2.Size() << std::endl;
+    PrintVector(data2);
+    data2.EmplaceBack("C");
+    data2.PushBack("Java");
+    std::cout << data2.Size() << std::endl;
+    PrintVector(data2);
+    return 0;
+}
